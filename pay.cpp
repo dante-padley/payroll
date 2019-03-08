41523b8225be7b2c72452ff15cc280a0ad5fe703
Dante Padley <dante_padley@yahoo.com>
2019-03-07 17:17:18 -0800

    readData function done, start of getCompanies

--- pay.cpp
+++ pay.cpp
@@ -1,11 +1,44 @@
 #include <iostream>
-
+#include <vector>
+#include <fstream>
 #include "person.cpp"
 using namespace std;
 
+void readData(vector<Person> &employees /*, vector<string> &companyNames*/){
+    ifstream rw;
+    rw.open("input.txt");
 
+    int i = 0;
+    while(!rw.eof()){
+        string first, last, company;
+        int id;
+        float hours, pay;
+        Person dude;
 
+        rw >> first >> last >> id >> company >> hours >> pay;
+
+        dude.setFirstName(first);
+        dude.setLastName(last);
+        dude.setEmployeeId(id);
+        dude.setCompanyName(company);
+        dude.setHoursWorked(hours);
+        dude.setPayRate(pay);
+
+        employees.push_back(dude);
+        //companyNames.push_back(company);
+        
+        i++;
+    }
+    rw.close();
+}
+
+void getCompanies(vector<Person> &employees, vector<string> &companyNames){
+    
+}
 
 int main(){
-
+    vector<Person> employees;
+    vector<string> companyNames;
+    readData(employees/*, companyNames*/);
+    getCompanies(employees, companyNames);
 }
